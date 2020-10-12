6b51c1b9b89d377a78c82a033ee9b2db48e85a6e
Allythy <allythy@protonmail.com>
2020-10-12 14:03:45 -0300

    Add loguru

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-import telebot, datetime, time, random
+import random
+
+import telebot
+from loguru import logger
 from telebot import types
+
 from config import settings
 
 
 bot = telebot.TeleBot(settings.TOKEN)
 
 
-def CheckDiceCall(d):
+def check_dice_call(d):
     try:
         if d[0] == "*" and d[-1] == "*" and d[1] == "d" and d[2:-1].isnumeric():
             return True
         else:
             return False
-    except:
-        print("Error getting from mesage: ", d)
+    except NameError:
+        logger.exception(f"Error getting from mesage: {d}")
         return False
 
 
 @bot.message_handler(commands=["d100"])
 def d100(message):
-    print("d100")
     roll = random.randrange(1, 101)
     msg = f"Rolagem: {roll}"
     bot.send_message(message.chat.id, msg, reply_to_message_id=message.message_id)
+    logger.info(f"Chat: {message.chat.id}, Command: /d100, Roll: {roll}")
 
 
 @bot.message_handler(commands=["dicemenu"])
@@ -36,17 +40,20 @@ def dicemenu(message):
     rkm.row(dices[4], dices[5], dices[6])
     msg = "Escolha um dado para rolar"
     bot.send_message(message.chat.id, msg, reply_markup=rkm)
+    logger.info(f"Chat: {message.chat.id}, Command: /dicemenu")
 
 
 @bot.message_handler(
-    func=lambda message: CheckDiceCall(message.text), content_types=["text"]
+    func=lambda message: check_dice_call(message.text), content_types=["text"]
 )
 def bot_pedidos(message):
     dice = int(message.text[2:-1]) + 1
     roll = random.randrange(1, dice)
     msg = f"Rolagem: {roll}"
     bot.send_message(message.chat.id, msg, reply_to_message_id=message.message_id)
+    logger.info(f"Chat: {message.chat.id}, Roll: {roll}")
 
 
-print("Starting Telegram Bot")
-bot.polling(none_stop=True, interval=0)
+if __name__ == "__main__":
+    logger.info("Starting Telegram Bot")
+    bot.polling(none_stop=True, interval=0)
